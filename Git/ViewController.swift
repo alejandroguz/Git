93cbdc1ddfba88eeadb0db3035391b7ac5680fc2
Alejandro Guzman <ketokunxd@gmail.com>
2023-01-06 10:47:34 -0500

    Add new code in viewController

--- Git/ViewController.swift
+++ Git/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cow4: UIButton!
+    @IBOutlet weak var cow3: UIButton!
+    @IBOutlet weak var cow2: UIButton!
+    @IBOutlet weak var cow1: UIButton!
     @IBOutlet weak var nameLabel: UILabel!
 
     override func viewDidLoad() {
